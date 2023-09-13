7660462a05f39351f44558ee560c3a6f9e99bfd4
mrsaif888 <105549569+mrsaif888@users.noreply.github.com>
2023-09-12 23:04:08 -0700

    change in nutrition Values

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php 
 $name  = 'Saif';
 $price = 10;
+
+/* assosiative arrays*/
+$nutrition = [
+    'fat'   => 38, 
+    'sugar' => 51, 
+    'salt'  => 0.25,
+  ];
+  $nutrition['fat']   = 36;
+  $nutrition['fiber'] = 2.1;
+  $nutrition['protien'] = 7.3;
+  
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,5 +25,12 @@ $price = 10;
     <h2>Welcome <?php echo $name; ?></h2>
     <p>The cost of your candy is 
        $<?php echo $price; ?> per pack.</p>
+
+       <h2>Nutrition (per 100g)</h2>
+    <p>Fat:   <?php echo $nutrition['fat']; ?>%</p>
+    <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
+    <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
+    <p>Fiber:  <?php echo $nutrition['fiber']; ?>%</p>
+    <p>Protien:  <?php echo $nutrition['protien']; ?>%</p>
   </body>
 </html>
